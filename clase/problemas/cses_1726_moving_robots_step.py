8370199ac622762ef259089514d4aaf1f710e263
GalileoCap <galileocapp@gmail.com>
2023-10-04 11:30:43 -0300

    OK: CSES 1726 Python and cheat

---
+++ clase/problemas/cses_1726_moving_robots_step.py
@@ -0,0 +1,94 @@
+import numpy as np
+import itertools as itt
+
+def colPos(x, y, sz):
+  return x + sz * y
+
+def genMatrix(sz):
+  mat = np.zeros((sz**2, sz**2))
+
+  def getAmnt(x, y):
+    if x in [0, sz-1] and y in [0, sz-1]: # On a corner
+      return 1/2
+    elif x in [0, sz-1] or y in [0, sz-1]: # On a side (not corner)
+      return 1/3
+    else: return 1/4
+
+  for i, (y, x) in enumerate(itt.product(range(sz), range(sz))):
+    amnt = getAmnt(x, y)
+
+    if x != 0:
+      mat[colPos(x-1, y, sz), i] = amnt
+    if x != sz-1:
+      mat[colPos(x+1, y, sz), i] = amnt
+
+    if y != 0:
+      mat[colPos(x, y-1, sz), i] = amnt
+    if y != sz-1:
+      mat[colPos(x, y+1, sz), i] = amnt
+
+  return mat
+
+def printRow(row):
+  print('  {', end = '')
+  for i in range(len(row)-1):
+    print(row[i], ', ', end = '')
+  print(row[-1], end = '')
+  print('}', end = '')
+
+def printMatrix(mat):
+  print('{')
+  for i in range(len(mat)-1):
+    printRow(mat[i])
+    print(',')
+  printRow(mat[-1])
+  print('')
+  print('}')
+
+def matrixAfter(k, sz = 8):
+  return np.linalg.matrix_power(genMatrix(sz), k)
+
+def simulate(k, i, sz = 8):
+  mat = matrixAfter(k, sz)
+  startingArr = np.zeros(sz**2)
+  startingArr[i] = 1
+  return mat @ startingArr
+
+def solve(k, sz = 8):
+  odds = [simulate(k, i, sz) for i in range(sz**2)]
+
+  res = 0
+  for (y, x) in itt.product(range(sz), range(sz)):
+    foo = 1
+    for i in range(sz**2):
+      foo *= 1 - odds[i][colPos(x, y, sz)]
+    res += foo
+
+  return res
+
+def cses():
+  k = int(input())
+  res = solve(k, 8)
+  print(f'{res:.6f}')
+  
+def test():
+  try:
+    while True:
+      k = int(input())
+      if k == -1:
+        break
+
+      expected = float(input())
+      res = solve(k)
+      print(f'{expected:.6f}, {res:.6f}')
+  except EOFError:
+    return
+
+def cheat():
+  res = [solve(k, 8) for k in range(100 + 1)]
+  printRow(res)
+
+# printMatrix(genMatrix(8))
+# cses()
+# test()
+cheat()
